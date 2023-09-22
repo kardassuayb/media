bdb05b423e29deac9fcf2c480ae18c06a237ec4e
Suayb Kardas <kardassuayb@gmail.com>
2023-09-23 01:57:26 +0300

    Adding a Skeleton

---
+++ src/components/Skeleton.js
@@ -0,0 +1,18 @@
+import classNames from "classnames";
+
+const Skeleton = ({ times }) => {
+  //   const boxes = [];
+  //   for (let i = 0; i < times; i++) {
+  //     boxes.push(<div key={i} />);
+  //   }
+  //   return boxes;
+
+  const boxes = Array(times)
+    .fill(0)
+    .map((_, i) => {
+      return <div key={i} />;
+    });
+  return boxes;
+};
+
+export default Skeleton;
