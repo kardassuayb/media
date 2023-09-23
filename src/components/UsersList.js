84b44a64d3363d82f5b570e965aaf62f7c466ef4
Suayb Kardas <kardassuayb@gmail.com>
2023-09-23 14:15:10 +0300

    Creating new Users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 const UsersList = () => {
   const dispatch = useDispatch();
@@ -14,6 +15,10 @@ const UsersList = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
@@ -21,7 +26,25 @@ const UsersList = () => {
     return <div>Error fetching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
