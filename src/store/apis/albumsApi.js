460ae476b0225e736096ca321368efe5558a6711
Suayb Kardas <kardassuayb@gmail.com>
2023-09-26 00:34:40 +0300

    creating an endpoint for RTK Query

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,5 +1,27 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const albumsApi = createApi({
   reducerPath: "albums", // required
+  baseQuery: fetchBaseQuery({
+    // required
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    // required
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
 });
+
+export { albumsApi };
